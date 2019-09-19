9de3765750bb5f07e39836e813ab71e0c272dda2
Milad Ranjbar <milad@adakerja.com>
2019-09-19 22:19:17 +0430

    init project

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "https://49cd9ad5aa7749a597db7d5886739a1b@sentry.io/1370170"
+});
+app.use(Sentry.Handlers.requestHandler());
+
+app.get("/", (req, res, next) => {
+  res.send("hi world");
+});
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+app.use(Sentry.Handlers.errorHandler());
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log("start server on port: " + port);
+});
